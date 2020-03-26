f6d57ad384f661790e4ad5e0313f1c125917ba08
meyirbekovnaa <58616258+meyirbekovnaa@users.noreply.github.com>
2020-03-26 20:39:05 +0600

    hackerrank

---
+++ hackerrank/4.py
@@ -0,0 +1,9 @@
+# Python: Division
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(str(a//b))
+    print(str(a/b))
+
---
+++ hackerrank/9.py
@@ -0,0 +1,9 @@
+# Find the Runner-Up Score!
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(set(list(arr)))
+    ar = len(arr)
+    arr = sorted(arr)
+    print(arr[ar-2])

f2257e693ee31acd9619c7b9e738ce376ceda759
meyirbekovnaa <58616258+meyirbekovnaa@users.noreply.github.com>
2020-03-28 00:54:04 +0600

    Update 7.py

--- hackerrank/7.py
+++ hackerrank/7.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 
-if __name__ == '__main__':
-    n = int(input())
-    for i in range (1, n + 1):
-        print(i, end = '')
+n = int(raw_input())
+for i in xrange(n):
+  print (i+1,end="")
